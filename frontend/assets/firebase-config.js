9d8fe0b7e00ca53c8aa9cd922fe36c1e05f0d1de
raj <raj@rajlabs.in>
2026-04-18 00:21:48 +0530

    firebase go boom

--- frontend/assets/firebase-config.js
+++ frontend/assets/firebase-config.js
@@ -1,11 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyCRZ4wT4PXbyVSD1RGFC9OB75jrT5yruGU",
-  authDomain: "indusind-5529e.firebaseapp.com",
-  projectId: "indusind-5529e",
-  storageBucket: "indusind-5529e.firebasestorage.app",
-  messagingSenderId: "12427202949",
-  appId: "1:12427202949:web:0090d6605c8baddfc6f4ec",
-  measurementId: "G-T2LPT1GBEF"
+  apiKey: "AIzaSyCHzMhjOePcNBkvCpJE0H-S2jZ7q9cgaaE",
+  authDomain: "scorepredictor-9dd45.firebaseapp.com",
+  projectId: "scorepredictor-9dd45",
+  storageBucket: "scorepredictor-9dd45.firebasestorage.app",
+  messagingSenderId: "555373343943",
+  appId: "1:555373343943:web:64e8233090daa9a7043ae2",
+  measurementId: "G-N10HQR1382"
 };
-
 export const isFirebaseConfigured = true;
